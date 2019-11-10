3bb810292f0caf4cc9a90c339ed709b844bd460f
yinyuWu <2936932419@qq.com>
2019-11-10 19:28:39 +1100

    almost finished

--- ass3/q8.py
+++ ass3/q8.py
@@ -2,6 +2,42 @@
 
 import cs3311
 import sys
+
+def deleteMeetings(id, timetable):
+    for t in timetable:
+        if t[1] == id:
+            timetable.remove(t)
+
+
+def calculate_total_hours(days, timetable):
+    total = 0
+    for d in days:
+        total += 2
+        start = 10000
+        end = 0
+        for t in timetable:
+            if t[3] == d and t[4] < start:
+                start = t[4]
+            if t[3] == d and t[5] > end:
+                end = t[5]
+        end = (end//100)*60 + end%100
+        start = (start//100)*60 + start%100
+        total += (end-start)/60.0
+    return total
+
+def meeting_check(to_add, timetable):
+    for t in timetable:
+        # to_add is not the same day, before t, after t allowed
+        if to_add[3] != t[3]:
+            continue
+        if to_add[3] == t[3] and to_add[5] <= t[4]:
+            continue
+        if to_add[3] == t[3] and to_add[4] >= t[5]:
+            continue
+        else:
+            return False
+    return True
+
 conn = cs3311.connect()
 
 cur = conn.cursor()
@@ -41,20 +77,32 @@ for course in args:
         q = 'select distinct id from q8 where code = %s and name = %s'
         cur.execute(q, q_list)
         cl = cur.fetchall()
-        # insert different classes into list
+        # insert different classes' code into list
         diff_class = []
         for c in cl:
             diff_class.append(c[0])
         #print(diff_class)
         # insert meetings into timetable
-        q = 'select * from q8 where id = %s'
-        cur.execute(q, [diff_class[0]])
-        res = cur.fetchall()
-        # insert meetings to class table
-        for each in res:
-            timetable.append(each)
-            hour = float(each[-1])
-            total_hours += hour
+        for dc in diff_class:
+            q = 'select * from q8 where id = %s'
+            cur.execute(q, [dc])
+            res = cur.fetchall()
+            success_cnt = 0
+            # insert meetings to class table
+            for each in res:
+                if (meeting_check(each, timetable) == False):
+                    # if fail to insert, delete all classes inserted
+                    deleteMeetings(dc, timetable)
+                    break
+                timetable.append(each)
+                success_cnt += 1
+            # if all meetings are inserted successfully
+            if (success_cnt == len(res)):
+                break
+            
+
+
+
 # print timetable
 day_dic = {"Mon" : 1, "Tue" : 2, "Wed" : 3, "Thu" : 4, "Fri" : 5}
 
@@ -65,6 +113,9 @@ for time in timetable:
 days = set(days)
 days = list(days)
 days = sorted(days, key= lambda x: day_dic[x])
+
+# calculate total hours
+total_hours = calculate_total_hours(days, timetable)
 
 print("Total hours: {:.1f}".format(total_hours))
 for d in days:
